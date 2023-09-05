5518dad34d1f9a0580e99a7cbe589550767bd89f
Vlad Frolov <frolvlad@gmail.com>
2023-09-05 16:29:44 +0200

    fix: Fixed imports in build.js after recent change of module type in package.json

--- build.js
+++ build.js
@@ -1,7 +1,5 @@
-const fs = require("fs");
-const replaceInFiles = require("replace-in-files");
-
-const packageJson = require('./package.json');
+import fs from "fs";
+import replaceInFiles from "replace-in-files";
 
 const transpiledPathPrefix = ".bos/transpiled/src/npm_package_name";
 
@@ -9,24 +7,31 @@ async function build() {
   await replaceInFiles({
     files: [`${transpiledPathPrefix}/**/*.jsx`],
     from: /export\s+default\s+function[^(]*\((.*)/gms,
-    to: (_match, rest) => `function MainComponent(${rest}\nreturn MainComponent(props, context);`,
+    to: (_match, rest) =>
+      `function MainComponent(${rest}\nreturn MainComponent(props, context);`,
   });
 
   await replaceInFiles({
     files: [`${transpiledPathPrefix}/**/*.jsx`],
     from: /^export /,
     // NOTE: Empty string is ignored, so we use a function workaround it
-    to: () => ''
-  })
-  
+    to: () => "",
+  });
+
   // WARNING: Don't allow "imports" in includes as this may lead to undefined
   // behavior as replacements are done in parallel and one file may be getting
   // replacements saved while the other file needs to include it, which ends up
   // with empty content includes.
   await new Promise((resolve) => {
-    fs.rename(`${transpiledPathPrefix}/includes`, `${transpiledPathPrefix}/../includes`, () => { resolve() });
+    fs.rename(
+      `${transpiledPathPrefix}/includes`,
+      `${transpiledPathPrefix}/../includes`,
+      () => {
+        resolve();
+      }
+    );
   });
-  
+
   await replaceInFiles({
     files: [`${transpiledPathPrefix}/**/*.jsx`],
     from: /import .* from "@\/includes\/([^"]*)";/gms,
@@ -39,14 +44,24 @@ async function build() {
     },
   });
 
+  const packageJson = JSON.parse(
+    fs.readFileSync(new URL("./package.json", import.meta.url))
+  );
+
   await replaceInFiles({
     files: [`${transpiledPathPrefix}/**/*.jsx`],
     from: /^/m,
     to: `/*\nLicense: ${packageJson.license}\nAuthor: ${packageJson.author}\nHomepage: ${packageJson.homepage}\n*/\n`,
-  })
+  });
 
   await new Promise((resolve) => {
-    fs.rename(transpiledPathPrefix, `.bos/transpiled/src/${packageJson.name}`, () => { resolve() });
+    fs.rename(
+      transpiledPathPrefix,
+      `${transpiledPathPrefix}/../${packageJson.name}`,
+      () => {
+        resolve();
+      }
+    );
   });
 
   console.log("DONE");
